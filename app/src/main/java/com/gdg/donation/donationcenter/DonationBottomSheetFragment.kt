a1d1cc9a44c3b7b26fad1ca8bd51c4c41c68e6e2
sunflow-er <antjd03170@gmail.com>
2024-11-15 21:50:07 +0900

    feat : 기부 바텀 시트 구성

---
+++ app/src/main/java/com/gdg/donation/donationcenter/DonationBottomSheetFragment.kt
@@ -0,0 +1,25 @@
+package com.gdg.donation.donationcenter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gdg.donation.R
+import com.gdg.donation.databinding.BottomSheetDonationBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class DonationBottomSheetFragment : BottomSheetDialogFragment() {
+    private lateinit var binding : BottomSheetDonationBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = BottomSheetDonationBinding.inflate(inflater, container, false)
+        val view = binding.root
+        view.setBackgroundResource(R.drawable.bottom_sheet_background)
+        return view
+    }
+    
+}
