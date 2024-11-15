8d8882b60c58a98aeabd113dc7bc7dbfefd17cb5
JinVibe <loonaticvibe2.11@gmail.com>
2024-11-16 06:36:23 +0900

    feat: 각종 api 연결 완료

--- app/src/main/java/com/gdg/donation/api/signup/RetrofitInstance.kt
+++ app/src/main/java/com/gdg/donation/api/signup/RetrofitInstance.kt
@@ -1,10 +1,15 @@
 package com.gdg.donation.api
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
 
     private val client = OkHttpClient.Builder()
         .addInterceptor { chain ->
@@ -17,11 +22,15 @@ object RetrofitInstance {
 
     private val retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("http://192.168.10.123:8080") // 서버의 기본 URL
+            .baseUrl("http://3.38.223.188:8080/") // 서버의 기본 URL
             .client(client) // 헤더를 추가한 OkHttpClient 사용
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
+
+    private val httpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
 
     val api: ApiService by lazy {
         retrofit.create(ApiService::class.java)
