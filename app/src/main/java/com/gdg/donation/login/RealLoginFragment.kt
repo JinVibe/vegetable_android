4ca70b852aa110ef5215f8c62a2a7e6413d9bf3e
sunflow-er <antjd03170@gmail.com>
2024-11-16 06:33:30 +0900

    chore : retrofit2 주석 처리, GROWN 수정

--- app/src/main/java/com/gdg/donation/login/RealLoginFragment.kt
+++ app/src/main/java/com/gdg/donation/login/RealLoginFragment.kt
@@ -2,6 +2,7 @@ package com.gdg.donation.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.telecom.Call
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,9 +17,9 @@ import com.gdg.donation.*
 import com.gdg.donation.api.RetrofitInstance
 import com.gdg.donation.api.signup.request.LogInReqDTO
 import com.gdg.donation.api.signup.response.LogInResDTO
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+//import retrofit2.Call
+//import retrofit2.Callback
+//import retrofit2.Response
 
 
 class RealLoginFragment : Fragment() {
@@ -41,11 +42,11 @@ class RealLoginFragment : Fragment() {
         val textViewSignUp = view.findViewById<TextView>(R.id.textViewSignUp)
 
         buttonLogin.setOnClickListener {
-             login()
+//             login()
 
-//            parentFragmentManager.commit {
-//                moveToMainScreen()
-//            }
+            parentFragmentManager.commit {
+                moveToMainScreen()
+            }
         }
 
         buttonGoogleSignIn.setOnClickListener {
@@ -88,7 +89,7 @@ class RealLoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // EditText 초기화
-        editTextEmail = view.findViewById(R.id.editTextEmail)
+        editTextEmail = view.findViewById(R.id.editTextId)
         editTextPassword = view.findViewById(R.id.editTextPassword)
     }
 
@@ -103,20 +104,20 @@ class RealLoginFragment : Fragment() {
 
         val loginRequest = LogInReqDTO(email, password)
 
-        RetrofitInstance.api.logIn(loginRequest).enqueue(object : Callback<LogInResDTO> {
-            override fun onResponse(call: Call<LogInResDTO>, response: Response<LogInResDTO>) {
-                if (response.isSuccessful) {
-                    Toast.makeText(requireContext(), "로그인 성공", Toast.LENGTH_SHORT).show()
-                    moveToMainScreen()
-                } else {
-                    Toast.makeText(requireContext(), "로그인 실패: 이메일이나 비밀번호를 확인하세요.", Toast.LENGTH_SHORT).show()
-                }
-            }
-
-            override fun onFailure(call: Call<LogInResDTO>, t: Throwable) {
-                Toast.makeText(requireContext(), "네트워크 오류 발생", Toast.LENGTH_SHORT).show()
-            }
-        })
+//        RetrofitInstance.api.logIn(loginRequest).enqueue(object : Callback<LogInResDTO> {
+//            override fun onResponse(call: Call<LogInResDTO>, response: Response<LogInResDTO>) {
+//                if (response.isSuccessful) {
+//                    Toast.makeText(requireContext(), "로그인 성공", Toast.LENGTH_SHORT).show()
+//                    moveToMainScreen()
+//                } else {
+//                    Toast.makeText(requireContext(), "로그인 실패: 이메일이나 비밀번호를 확인하세요.", Toast.LENGTH_SHORT).show()
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<LogInResDTO>, t: Throwable) {
+//                Toast.makeText(requireContext(), "네트워크 오류 발생", Toast.LENGTH_SHORT).show()
+//            }
+//        })
     }
 
     private fun moveToMainScreen() {
